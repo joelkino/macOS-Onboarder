bf684732927603ef157715b1cf548467e546ff3a
joelkino <55217583+joelkino@users.noreply.github.com>
2024-09-04 15:49:34 +1000

    add notes

--- macOS-Onboarder/scripts/RetrievePWSerialCipher.sh
+++ macOS-Onboarder/scripts/RetrievePWSerialCipher.sh
@@ -1,3 +1,6 @@
+## This script can be run on any macOS device and does not need to be run on the device you are retireving the password for
+## Please note that this method is not entirely secure
+
 echo -ne "Enter device serial number :"
 read serial
 password=`echo $serial | tr '[A-Z]' '[K-ZA-J]' | tr 0-9 4-90-3 | base64`
