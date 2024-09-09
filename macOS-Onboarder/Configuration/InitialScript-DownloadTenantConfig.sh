a000728f1903acc08bad18d1a5e08b5c66b55950
joelkino <55217583+joelkino@users.noreply.github.com>
2024-09-09 20:39:10 +1000

    updates

--- macOS-Onboarder/Configuration/InitialScript-DownloadTenantConfig.sh
+++ macOS-Onboarder/Configuration/InitialScript-DownloadTenantConfig.sh
@@ -4,36 +4,26 @@
 PLATFORM_SSO_PLIST="/Library/Managed Preferences/com.apple.extensiblesso.plist"
 ACCOUNT_DISPLAY_NAME=$(defaults read "$PLATFORM_SSO_PLIST" "AccountDisplayName")
 
-# Check if AccountDisplayName is empty
+## Check if AccountDisplayName is empty
 if [ -z "$AccountDisplayName" ]; then
   echo "AccountDisplayName not found."
   exit 1
 fi
 
-# Step 2: Map AccountDisplayName to a specific tenant
-case "$AccountDisplayName" in
-  "x")
-    TENANT="Tenant1"
-    ;;
-  "y")
-    TENANT="Tenant2"
-    ;;
-  "z")
-    TENANT="Tenant3"
-    ;;
-  *)
-    echo "AccountDisplayName does not match any valid tenants."
-    exit 1
-    ;;
-esac
+# Step 3: Compare AccountDisplayName against the JSON entries
+TENANT=$(grep -A 1 "\"AccountDisplayName\": \"$ACCOUNT_DISPLAY_NAME\"" "$JSON_FILE" | grep "ShortName" | awk -F ': ' '{print $2}' | tr -d '",')
 
-# Step 3: Determine the URL for the specific plist file based on ACCOUNT_DISPLAY_NAME
+# Check if TENANT is empty
+if [ -z "$TENANT" ]; then
+  echo "AccountDisplayName does not match any valid tenants."
+  exit 1
+fi
 
-## Updte the BASE_URL with the URL of the repository ##
-BASE_URL="https://github.com/joelkino/macOS-Onboarder/tree/main/macOS-Onboarder/Configuration/Tenant-Plists"
-PLIST_URL="$BASE_URL/mIOU-$AccountDisplayName.plist"
+# Step 4: Determine the URL for the specific plist file based on TENANT
+BASE_URL="https://github.com/joelkino/macOS-Onboarder/raw/main/macOS-Onboarder/Configuration/Tenant-Plists"
+PLIST_URL="$BASE_URL/mIOU-$TENANT.plist"
 
-# Step 4: Download the specific plist file
+# Step 5: Download the specific plist file
 DOWNLOAD_PATH="/Library/Preferences/com.secondsonconsulting.baseline.plist"
 curl -o "$DOWNLOAD_PATH" "$PLIST_URL"
 
